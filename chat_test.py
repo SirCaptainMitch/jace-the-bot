568a49c613376bfd893894c7db2254369aca0781
SirCaptainMitch <hamann.mitchell@gmail.com>
2023-07-27 12:59:14 -0500

    updates

--- chat_test.py
+++ chat_test.py
@@ -9,26 +9,10 @@ console = Console()
 # Chat(agent).start()
 
 # console.print(GPT4All.list_models())
-model = GPT4All("wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin")
-prompt = f"Tell me the story of the Edmund Fitzgerald"
-template = f"""
-A chat between a curious user and artificial intelligence assistant. The assistant gives helpful, detailed, and polite answers to the user's questions and takes on the persona of a Minnesotan using the local dialect, slang, and colloquialisms. 
+model_name = 'ggml-model-gpt4all-falcon-q4_0.bin'
+model = GPT4All(model_name)
+prompt = f"Tell me about yourself"
+prompt_template = f"### Instruction: You are a Minnesotan and respond in a Minnesota dialect, colloquialisms, and slang {prompt}\n### Response:"
+console.print(prompt)
+console.print(model.generate(prompt=prompt, max_tokens=260, top_k=1))
 
-USER: {prompt}
- 
-ASSISTANT: 
-"""
-console.print(template)
-console.print(model.generate(prompt=prompt, max_tokens=200, top_k=1))
-# tokens = list(model.generate(prompt="The capital of France is ", streaming=True))
-# console.print(tokens)
-# console.print({'role': 'assistant', 'content': ''.join(tokens)})
-
-# with model.chat_session():
-#     user = model.generate(prompt=template)
-#     model.current_chat_session.append({'role': 'assistant', 'content': ''.join(tokens)})
-#
-#     tokens = list(model.generate(prompt='write me a poem about dogs', top_k=1, streaming=True))
-#     model.current_chat_session.append({'role': 'assistant', 'content': ''.join(tokens)})
-#
-#     console.print(model.current_chat_session)
